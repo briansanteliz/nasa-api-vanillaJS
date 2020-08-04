59cb536a31c0ebc7e0819a437e78690606616d21
Brian <blsvsanteliz@gmail.com>
2020-08-03 23:15:27 -0400

    Building Footer

--- js/Ui.js
+++ js/Ui.js
@@ -3,7 +3,7 @@ export class Ui{
         let html = `
             <div class="shadow-lg  mb-4 bg-white rounded card mb-3 ">
                     
-                    <img src="${data.url}" class="img-fluid card-img-top" alt="${data.title}" loading="lazy">
+                    <img src="${data.url}" class="img-fluid card-img-top" alt="${data.title}">
                    
                        <div class="card-body" style="background:#212121; color:#dadada;">
                        <h2 class="card-title my-3 ">
@@ -12,7 +12,6 @@ export class Ui{
                            <p class="card-text">${data.explanation}</p>
                        
                            <div class="card-text text-right p-1 d-flex justify-content-between">
-                           <small class="p-1" style="color:#bfbfbf !important">Fecha: ${data.date}</small> 
                            <small class="p-1" style="color:#bfbfbf !important">Fecha: ${data.date}</small> 
                           <small class="p-1" style="color:#bfbfbf !important">Autor: ${data.copyright ? data.copyright.substring(0,13): 'NASA'}.</small> 
                       </div>
--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 import {Api} from './Api.js'
 import {Ui} from './Ui.js'
+
+const dateInput = document.getElementById('fecha');
+const form = document.getElementById('form')
+const year = document.getElementById('year')
+
 const container = document.getElementById('container')
 const api = new Api()
 const ui = new Ui()
@@ -9,8 +14,8 @@ document.addEventListener('DOMContentLoaded', async()=>{
    ui.Template(data,container)
       
 })
-const dateInput = document.getElementById('fecha');
-const form = document.getElementById('form')
+ const año =  new Date().getFullYear()
+ year.innerText = año
 form.addEventListener('submit', (e)=>{
     console.log(dateInput.value)
     form.reset()
